796cb8c2bc0e220a63879421207116969149e833
Denitsa Velikova <105812554+deniitooou@users.noreply.github.com>
2023-01-20 16:02:26 +0200

    CRUD - Exercises
    CRUD - Exercises

---
+++ CRUD - Exercises/24. Countries and Currency.sql
@@ -0,0 +1,9 @@
+USE [Geography]
+
+SELECT [CountryName], [CountryCode],
+  CASE WHEN [CurrencyCode] = 'EUR' 
+       THEN 'Euro'
+  ELSE 'Not Euro'
+   END AS [Currency]
+  FROM [Countries]
+ ORDER BY [CountryName]
