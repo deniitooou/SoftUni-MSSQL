6203990b0e3a21db02c799d054ef7d76997151cd
Denitsa Velikova <105812554+deniitooou@users.noreply.github.com>
2023-01-20 15:58:26 +0200

    CRUD - Exercises
    CRUD - Exercises

---
+++ CRUD - Exercises/17. Create View Employees with Job Titles.sql
@@ -0,0 +1,7 @@
+USE [SoftUni]
+
+CREATE VIEW V_EmployeeNameJobTitle AS
+SELECT 
+	[FirstName] + ' ' + ISNULL([MiddleName], '') + ' ' + [LastName] AS [Full Name]
+	,[JobTitle] AS [Job Title]
+ FROM [Employees]
