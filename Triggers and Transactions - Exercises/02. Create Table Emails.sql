7acdad1c4e51d6c704a780c917fec3d5aeeef8e0
Denitsa Velikova <105812554+deniitooou@users.noreply.github.com>
2023-02-13 17:51:51 +0200

    Triggers and Transactions - Exercises
    Triggers and Transactions - Exercises

---
+++ Triggers and Transactions - Exercises/02. Create Table Emails.sql
@@ -0,0 +1,17 @@
+CREATE TRIGGER tr_CreateNewEmailOnNewLogEntry
+ON [Logs] FOR INSERT
+AS
+INSERT INTO [NotificationEmails] 
+   VALUES
+(
+	(SELECT [AccountId] FROM inserted)
+	,(SELECT 'Balance change for account: ' + CAST([AccountId] AS VARCHAR(255)) FROM inserted)
+	,(SELECT 'On ' + 
+			FORMAT(GETDATE(), 'MMM dd yyyy h:mmtt') + 
+			' your balance was changed from ' + 
+			CAST([OldSum] AS VARCHAR(255)) + 
+			' to ' + 
+			CAST([NewSum] AS VARCHAR(255)) + 
+			'.' 
+	FROM inserted)
+)
